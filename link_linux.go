419497a0ef5d4d5be325ca8467f91db12a98fae3
Dinesh Majrekar <dinesh@civo.com>
2023-03-13 11:16:58 +0000

    Move IGNORE_DF

--- link_linux.go
+++ link_linux.go
@@ -2756,7 +2756,7 @@ func addGretapAttrs(gretap *Gretap, linkInfo *nl.RtAttr) {
 	data.AddRtAttr(nl.IFLA_GRE_ENCAP_FLAGS, nl.Uint16Attr(gretap.EncapFlags))
 	data.AddRtAttr(nl.IFLA_GRE_ENCAP_SPORT, htons(gretap.EncapSport))
 	data.AddRtAttr(nl.IFLA_GRE_ENCAP_DPORT, htons(gretap.EncapDport))
-	data.AddRtAttr(nl.IFLA_GRE_IGNORE_DF, nl.Uint16Attr(gretap.IgnoreDf))
+	data.AddRtAttr(nl.IFLA_GRE_IGNORE_DF, nl.Uint16Attr(gretap.IgnoreDF))
 }
 
 func parseGretapData(link Link, data []syscall.NetlinkRouteAttr) {
